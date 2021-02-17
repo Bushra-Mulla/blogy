3d7e8099793d42bc6e7a2555c740b97b59b0588f
ashwagzabani <ashwag.amz@gmail.com>
2021-02-17 15:33:06 +0300

    add urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('logIn/', views.logIn, name='blog-logIn'),
+    path('signup/', views.signup, name='blog-signup'),
 ]
