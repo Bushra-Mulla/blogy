cd5664c58e7fda6bd51354197aa62bfed07c814c
fatmahhelal <ana.fatim16@gmail.com>
2021-02-18 02:36:43 +0300

    Slider, Create & edit post

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('logIn/', views.logIn, name='blog-logIn'),
     path('signup/', views.signup, name='blog-signup'),
-    path('create/', views.PostCreate.as_view(), name='blog-post-create'),
-    path('<int:pk>/update/', views.PostUpdate.as_view(), name='blog-post-Update'),
+    path('post/create/', views.PostCreate.as_view(), name='blog-post-create'),
+    path('post/<int:pk>/update/', views.PostUpdate.as_view(), name='blog-post-Update'),
 ]
 
