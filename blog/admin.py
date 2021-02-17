52ad94cbec66dc78487ed01b5fae5230a3a59ffa
fatmahhelal <ana.fatim16@gmail.com>
2021-02-17 19:07:23 +0300

    done

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,4 @@ from .models import *
 admin.site.register(user_profile)
 admin.site.register(categorys)
 admin.site.register(Post)
-# admin.site.register(likes)
+admin.site.register(likes)
--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,6 @@ class user_profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.CharField(max_length=255)
     about_me = models.CharField(max_length=255)
-    position = models.CharField(max_length=100)
 
 class categorys(models.Model):
     category_name = models.CharField(max_length=100)
@@ -30,7 +29,10 @@ class Post(models.Model):
 class likes(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     Post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
-    likes_count = models.IntegerField(default=0)
+    likes_count = models.IntegerField()
+
+    def __str__(self):
+        pass
 
 
 class comment(models.Model):
@@ -38,6 +40,10 @@ class comment(models.Model):
     Publish_date = models.DateTimeField(default=timezone.now)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     Post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        pass
+        # ordering = []
 
 
 class report(models.Model):
