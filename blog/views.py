f0a023a509659690f04addad176ed261c9b02126
29_Bushra Win_SEI-14 <bushra.1416@hotmail.com>
2021-02-18 10:03:56 +0300

    ignore file

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,9 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 def home (request):
-    last_twenty = Post.objects.all().order_by('-id')[:20]
+    # last_twenty = Post.objects.all().order_by('-id')[:20]
+    last_twenty = Post.objects.filter(isPublish= True).order_by('-id')[:20]
+
     related = Post.objects.select_related('author').all()
     # last_twenty=Post.objects.all()
     return render(request, 'index.html', {'posts': last_twenty, 'related': related})
--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'blog.apps.BlogConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
