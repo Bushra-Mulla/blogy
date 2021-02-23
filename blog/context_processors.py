032339344fb2396434ad95ee14c1ec24a6664cc1
29_Bushra Win_SEI-14 <bushra.1416@hotmail.com>
2021-02-24 02:53:54 +0300

    merge and pu;; 24

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -60,3 +60,52 @@ def add_variable_to_context(request):
     return {
         'categorys_list': categorys.objects.all()
     }
+
+
+
+
+## publish
+
+def countpost(request):
+    countpost = Post.objects.filter(isPublish='published').count()
+    return {'countpost': countpost}
+
+
+def countrefused (request):
+    countpost = Post.objects.filter(isPublish='refused').count()
+    return {'countrefused': countpost}
+
+
+def countnotPublished(request):
+    countpost = Post.objects.filter(isPublish = 'notPublished').count()
+    return {'countnotPublished': countpost}
+
+
+# def getAllNotArchivedReport(request):
+#     reports = report.objects.filter(is_archived=False).select_related(
+#         'user_id__user_profile').all().order_by('-id')
+#     return {'notArchivedReport': reports}
+
+
+# def getReports(report):
+#     return {'report': report}
+
+
+# def getAllArchivedReport(request):
+#     reports = report.objects.filter(is_archived=True).select_related(
+#         'user_id__user_profile').all().order_by('-id')
+#     print(reports)
+#     return {'archivedReport': reports}
+
+
+# def all(request):
+#     reports = report.objects.all().select_related(
+#         'user_id__user_profile').all().order_by('-id')
+#     return {'allReport': reports}
+
+# from .models import categorys
+
+# def add_variable_to_context(request):
+#     return {
+#         'categorys_list': categorys.objects.all()
+#     }
