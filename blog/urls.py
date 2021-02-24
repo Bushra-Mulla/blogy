39b2b5cadc3c5dc63336d0f3ab3dc159f5b5a5e5
29_Bushra Win_SEI-14 <bushra.1416@hotmail.com>
2021-02-24 20:58:33 +0300

    migrate to heroku

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from . import context_processors
-from django.contrib import admin
+# from django.contrib import admin
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -92,5 +92,5 @@ urlpatterns = [
 
 
     path('search/', views.search, name='search'),
-    path('admin/', admin.site.urls, name='admin'),
+#     path('admin/', admin.site.urls, name='admin'),
 ]
