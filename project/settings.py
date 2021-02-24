6d3a20832888bec435148ec0cf198a2d14d5f9fa
29_Bushra Win_SEI-14 <bushra.1416@hotmail.com>
2021-02-24 18:44:55 +0300

    ,,

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['blogyproject.herokuapp.com']
+ALLOWED_HOSTS = ['blogyproject.herokuapp.com', '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
