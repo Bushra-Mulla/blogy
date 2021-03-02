db6ad904996511ef88fb953ea4f1b64384f0fb18
29_Bushra Win_SEI-14 <bushra.1416@hotmail.com>
2021-03-02 23:06:20 +0300

    try media in heroku

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['blogyproject.herokuapp.com', '0.0.0.0', 'localhost', '127.0.0.1']
